c66cd89301ce571bc4a83e1da7f6d9c9f68bc954
Oleg <luzeninoleg2012@gmail.com>
2025-09-14 18:36:42 +0300

    added history

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -3,6 +3,7 @@ import { COUNTDOWN_STORAGE_KEY, PersistedCountdownState } from "./index";
 import { useState, useEffect } from "react";
 import { getFromStorage } from "../../utils/asyncstorage";
 import { format } from "date-fns";
+import { theme } from "../../theme";
 
 export default function HistoryScreen() {
   const [countdownState, setCountdownState] = useState<
@@ -20,26 +21,55 @@ export default function HistoryScreen() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={countdownState?.completedAtTimestamps}
-        renderItem={({ item }) => {
-          return (
-            <View>
-              <Text>{format(item, dateFormat)}</Text>
-            </View>
-          );
-        }}
-      ></FlatList>
-    </View>
+    <FlatList
+      data={countdownState?.completedAtTimestamps}
+      contentContainerStyle={styles.contentContainer}
+      style={styles.flatList}
+      ListHeaderComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text style={styles.emptyText}>No updates yet</Text>
+        </View>
+      }
+      renderItem={({ item }) => {
+        return (
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>{format(item, dateFormat)}</Text>
+          </View>
+        );
+      }}
+    ></FlatList>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
+    padding: 16,
+    marginTop: 10,
+  },
+  flatList: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: theme.colorWhite,
+  },
+  listItem: {
+    backgroundColor: theme.colorLightGray,
+    marginHorizontal: 16,
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  listItemText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  emptyText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 10,
+  },
+  listEmptyContainer: {
+    marginVertical: 18,
     alignItems: "center",
-    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
